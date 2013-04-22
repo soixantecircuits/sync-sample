7550e31364aa39433e978ce4b49de9206ecb6c0f
Shiyue Wang <shiyue.wang@me.com>
2013-04-22 20:36:15 +0200

    sync demo

--- Sync_Sample/www/js/index.js
+++ Sync_Sample/www/js/index.js
@@ -20,9 +20,12 @@ var app = {
     path: '',
     filter: /^\/(.*)\//,
     eventFolder: "",
+    db: undefined,
     // Application Constructor
     initialize: function () {
         this.bindEvents();
+
+
     },
     // Bind Event Listeners
     //
@@ -37,25 +40,26 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
 
     onDeviceReady: function () {
+        app.db = window.openDatabase("test", "1.0", "Test DB", 1000000);
+        app.db.transaction(function (tx) {
+            tx.executeSql('CREATE TABLE IF NOT EXISTS URLS (id unique, url, file)');
+        });
         var pathName = window.location.pathname.split("/"),
             json = "https://www.dropbox.com/s/kadx8a6zyhvinb9/links.json?dl=1",
             json2 = "https://www.dropbox.com/s/wot3zeg9ljnxsb5/links1.json?dl=1";
 
         pathName.splice(-3, 3);
         var documantFolder = window.location.protocol + '//' + pathName.join("/") + "/Documents";
-
-        app.updateFileList(json, "MasterPage",
-            {
-                success: function () {
-                    document.getElementById('picture').innerHTML = "<img src=\"" + documantFolder + "/MasterPage/Hello-World.png" + "\" />";
-                    document.getElementById('picture').style.display = "block";
-                    document.getElementById('movie').innerHTML = "<video width=\"320\" height=\"240\" controls><source src=\"" + documantFolder + "/MasterPage/small.mp4" + "\" type=\"video/mp4\"></video>";
-                    document.getElementById('movie').style.display = "block";
-                }
-            });
+        window.resolveLocalFileSystemURI(documantFolder + "/" + "MasterPage", function () {
+            var path = window.location.pathname.split("/");
+            path.pop();
+            window.location.replace(path.join("/") + "/page.html");
+        }, function () {
+            app.updateFileList(json, "MasterPage");
+        });
     },
 
-    readAsTextMaster: function (file, folderName, option) {
+    readFileList: function (file, folderName, option) {
         var reader = new FileReader();
         reader.onloadend = function (evt) {
             var long = evt.target.result,
@@ -65,20 +69,37 @@ var app = {
                 pathBis = "",
                 url = "",
                 elt = "",
-                fileTransfer = new FileTransfer();
+                fileTransfer = undefined;
+
+            $('.progressFile').empty().append("<h1>Still " + jsondata.CACHE.length + " files to go!</h1>");
             for (var cpt = 0; cpt < jsondata.CACHE.length; cpt++) {
                 elt = jsondata.CACHE[cpt];
-                if (elt != "") {
-                    url = option.baseUrl ? baseUrl + elt : elt;
-                    pathBis = app.path + folderName + "/" + elt.split('/').pop();
+
+                if (elt.url != "") {
+
+                    url = option.baseUrl ? baseUrl + elt.url : elt.url;
+                    pathBis = app.path + folderName + "/" + elt.url.split('/').pop()
+                    fileTransfer = new FileTransfer();
+                    fileTransfer.onprogress = function (progressEvent) {
+                        if (progressEvent.lengthComputable) {
+                            var percent = (progressEvent.loaded / progressEvent.total) * 600;
+                            $(".progressbar-inner").width(percent);
+                            //$(".progressbar-inner").width(percent);
+                        }
+                    };
                     fileTransfer.download(
                         url, pathBis, function (entry) {
                             numSuccess++;
-                            if (numSuccess === jsondata.CACHE.length) {
-                                option.success && option.success();
+                            $('.progressFile').empty().append("<h1>Still " + (jsondata.CACHE.length - numSuccess) + " files to go</h1>");
+                            if (entry.name.indexOf("mp4") != -1) {
+                                $('body').append("<video width=\"320\" height=\"240\" controls><source src=\"" + entry.fullPath + "\" type=\"video/mp4\"></video>");
                             }
+                            else if (entry.name.indexOf("png") != -1) {
+                                $('body').append("<img src=\"" + entry.fullPath + "\" />");
+                            }
+
                         }, function (error) {
-                            console.log("Download error, target: " + elt);
+                            console.log("Download error, target: " + elt.url);
                         });
                 }
             }
@@ -92,15 +113,15 @@ var app = {
 
     updateFileList: function (fileListUrl, folderName, option) {
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
-            fileSystem.root.getFile("manifest", {create: true}, function (fileEntry) {
+            fileSystem.root.getFile("fileList", {create: true}, function (fileEntry) {
                 app.path = app.filter.exec(fileEntry.fullPath)[0];
                 fileEntry.file(function (file) {
                     var fileTransfer = new FileTransfer();
-                    var manifestPath = app.path + "manifest";
+                    var fileListPath = app.path + "fileList";
                     fileTransfer.download(
-                        fileListUrl, manifestPath,
+                        fileListUrl, fileListPath,
                         function (entry) {
-                            app.readAsTextMaster(file, folderName, {
+                            app.readFileList(file, folderName, {
                                 success: option.success
                             });
                         }, function (error) {
@@ -109,5 +130,34 @@ var app = {
                 }, app.fail);
             }, app.fail);
         }, app.fail);
+    },
+    populateDB: function (tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS URLS (id unique, url, file)');
+        tx.executeSql('INSERT INTO URLS (id, data) VALUES (1, "First row")');
+        tx.executeSql('INSERT INTO URLS (id, data) VALUES (2, "Second row")');
+    },
+    showContent: function () {
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+            console.log("Root = " + fs.root.fullPath);
+            var directoryEntry = new DirectoryEntry("MasterPage", fs.root.fullPath + "/MasterPage");
+            var directoryReader = directoryEntry.createReader();
+            directoryReader.readEntries(function (entries) {
+                var i;
+                for (i = 0; i < entries.length; i++) {
+                    console.log(entries[i].name);
+                    if (entries[i].name.indexOf("mp4") != -1) {
+                        $('body').append("<video width=\"320\" height=\"240\" controls><source src=\"" + entries[i].fullPath + "\" type=\"video/mp4\"></video>");
+                    }
+                    else if (entries[i].name.indexOf("png") != -1) {
+                        $('body').append("<img src=\"" + entries[i].fullPath + "\" />");
+                    }
+
+                }
+            }, function (error) {
+                alert(error.code);
+            })
+        }, function (error) {
+            alert(error.code);
+        });
     }
 };
